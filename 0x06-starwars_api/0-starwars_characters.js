a1f4408d24bb8e7747b7e7bf696bfd3d7d7dc014
Boozmanras <paulvictoraa@gmail.com>
2024-11-15 05:59:57 +0300

    added

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,43 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-if (process.argv.length < 3) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error: ${response.statusCode}`);
-    return;
-  }
-
-  const film = JSON.parse(body);
-  film.characters.forEach(characterUrl => {
-    request(characterUrl, (err, resp, charBody) => {
-      if (err) {
-        console.error(err);
-        return;
+const makeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
       }
-
-      if (resp.statusCode !== 200) {
-        console.error(`Error: ${resp.statusCode}`);
-        return;
-      }
-
-      const character = JSON.parse(charBody);
-      console.log(character.name);
     });
   });
-});
+};
+
+async function getAndPrintCharacters () {
+  if (process.argv.length !== 3) {
+    console.error('Please provide a movie ID');
+    process.exit(1);
+  }
+
+  const movieId = process.argv[2];
+  const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+  try {
+    const filmData = await makeRequest(filmUrl);
+    const characters = filmData.characters;
+
+    for (const characterUrl of characters) {
+      try {
+        const character = await makeRequest(characterUrl);
+        console.log(character.name);
+      } catch (error) {
+        console.error(`Error fetching character: ${error.message}`);
+      }
+    }
+  } catch (error) {
+    console.error(`Error fetching movie data: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+getAndPrintCharacters();
